9b66301f8e4c007c41581fac62a9549a54220d00
Zeineb136 <moussa.zeineb1@gmail.com>
2022-07-12 15:26:04 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -11,18 +11,19 @@
 int _printf(const char *format, ...)
 {
 	int i, j, length, d_var, dvar_temp, dvar_length;
-	int* dvar_digits;
-	char* format_str;
+	int *dvar_digits;
+	char *format_str;
 	va_list arg;
+
 	va_start(arg, format);
-	if (format == NULL )
+	if (format == NULL)
 		return (-1);
 	length = 0;
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] =='%')
+		if (format[i] == '%')
 		{
-			switch(format[i+1])
+			switch (format[i + 1])
 			{
 			case 'c':
 				_putchar(va_arg(arg, int));
@@ -31,26 +32,29 @@ int _printf(const char *format, ...)
 				break;
 			case 'd':
 				d_var = va_arg(arg, int);
-				if(d_var<0){
+				if (d_var < 0)
+				{
 					_putchar('-');
 					length++;
 					d_var = -d_var;
 				}
 				dvar_temp = d_var;
-				while(dvar_temp){
+				while (dvar_temp)
+				{
 					dvar_length++;
-					dvar_temp/=10;
+					dvar_temp /= 10;
 				}
 				dvar_digits = malloc(dvar_length);
 				j = 0;
-				while(d_var){
-					dvar_digits[j] = d_var%10;
-					j++;
-					d_var/=10;
-				}
-				for (j = dvar_length-1; j>=0; j--)
+				while (d_var)
 				{
-					_putchar(dvar_digits[j]+'0');
+					dvar_digits[j] = d_var % 10;
+					j++;
+					d_var /= 10;
+				}
+				for (j = dvar_length - 1; j >= 0; j--)
+				{
+					_putchar(dvar_digits[j] + '0');
 					length++;
 				}
 				free(dvar_digits);
@@ -58,26 +62,29 @@ int _printf(const char *format, ...)
 				break;
 			case 'i':
 				d_var = va_arg(arg, int);
-				if(d_var<0){
+				if (d_var < 0)
+				{
 					_putchar('-');
 					length++;
 					d_var = -d_var;
 				}
 				dvar_temp = d_var;
-				while(dvar_temp){
+				while (dvar_temp)
+				{
 					dvar_length++;
-					dvar_temp/=10;
+					dvar_temp /= 10;
 				}
 				dvar_digits = malloc(dvar_length);
 				j = 0;
-				while(d_var){
-					dvar_digits[j] = d_var%10;
-					j++;
-					d_var/=10;
-				}
-				for (j = dvar_length-1; j>=0; j--)
+				while (d_var)
 				{
-					_putchar(dvar_digits[j]+'0');
+					dvar_digits[j] = d_var % 10;
+					j++;
+					d_var /= 10;
+				}
+				for (j = dvar_length - 1; j >= 0; j--)
+				{
+					_putchar(dvar_digits[j] + '0');
 					length++;
 				}
 				free(dvar_digits);
@@ -101,7 +108,7 @@ int _printf(const char *format, ...)
 			default:
 				_putchar(format[i]);
 				length = length + 1;
-			}	
+			}
 		}
 		else
 		{
