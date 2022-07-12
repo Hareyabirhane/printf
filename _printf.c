1959ff981d679accbc592496bb8a254bdcfb7660
Zeineb136 <104872308+Zeineb136@users.noreply.github.com>
2022-07-12 20:38:28 +0100

    printf file

---
+++ _printf.c
@@ -0,0 +1,81 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*_printf -prints according to format
+*@format: a character string
+*
+*Return: the number of character
+*/
+int _printf(const char *format, ...)
+{
+	int i, j, length, d_var, dvar_temp, dvar_length;
+	int* dvar_digits;
+	char* format_str;
+	va_list arg;
+	va_start(arg, format);
+	if (format == NULL )
+		return (-1);
+	length = 0;
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] =='%')
+		{
+			switch(format[i+1])
+			{
+			case 'c':
+				putchar(va_arg(arg, int));
+				i++;
+				break;
+			case 'd':
+				d_var = va_arg(arg, int);
+				dvar_temp = d_var;
+				while(dvar_temp){
+					dvar_length++;
+					dvar_temp/=10;
+				}
+				dvar_digits = malloc(dvar_length);
+				j = 0;
+				while(d_var){
+					dvar_digits[j] = d_var%10;
+					j++;
+					d_var/=10;
+				}
+				for (j = dvar_length-1; j>=0; j--)
+				{
+					putchar(dvar_digits[j]+'0');
+					length++;
+				}
+				free(dvar_digits);
+				i++;
+				break;
+			case 's':
+				format_str = va_arg(arg, char*);
+
+				for (j = 0; format_str[j]; j++)
+				{
+					putchar(format_str[j]);
+					length++;
+				}
+				i++;
+				break;
+			case '%':
+				putchar('%');
+				length++;
+				i++;
+				break;
+			default:
+				putchar(format[i]);
+				length = length + 1;
+			}	
+		}
+		else
+		{
+			putchar(format[i]);
+			length++;
+		}
+	}
+	va_end(arg);
+	return (length);
+}
