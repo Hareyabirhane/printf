183b27a530ca7204b7bb648eda6d8dc1219e307c
Hareyabirhane <104759727+Hareyabirhane@users.noreply.github.com>
2022-07-13 11:57:53 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,6 +2,33 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+int (*find_function(const char *format))(va_list)
+{
+	unsigned int i = 0;
+	code_f find_f[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_dec},
+		{"r", print_rev},
+		{"b", print_bin},
+		{"u", print_unsig},
+		{"o", print_octal},
+		{"x", print_x},
+		{"X", print_X},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+
+	while (find_f[i].sc)
+	{
+		if (find_f[i].sc[0] == (*format))
+			return (find_f[i].f);
+		i++;
+	}
+	return (NULL);
+}
 /**
 *_printf -prints according to format.
 *@format: a character string.
